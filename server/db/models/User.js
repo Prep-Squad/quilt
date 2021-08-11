245bd6e95a9aa71271b15e5b88014d14fa2d9786
Anna Strasser <albastrasser@gmail.com>
2021-08-11 15:44:28 -0400

    Update: firebase auth in progress

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,13 +2,8 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
-  email: {
+  firebaseId: {
     type: Sequelize.STRING,
-    validate: {
-      isEmail: true,
-      isUnique: true,
-    },
-    allowNull: false,
   },
   username: {
     type: Sequelize.STRING,
